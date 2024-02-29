e68e552e155442199f21e991cc499465b92f74cc
Alon-Barzilay <alonbarzilay0@gmail.com>
2024-02-28 20:54:49 -0600

    Added enums and getter and setter for toppingPrice.

--- Homework_5/src/pizzas/PizzaType.java
+++ Homework_5/src/pizzas/PizzaType.java
@@ -1,5 +1,26 @@
 package pizzas;
 
 public enum PizzaType {
+	HAWAIIAN(3.00),
+	MARGHERITA(2.50),
+	SUPREME(3.50),
+	VEGETARIAN(1.50);
+	
+	private double toppingPrice;
 
+    // Constructor
+    PizzaType(double toppingPrice) {
+        this.toppingPrice = toppingPrice;
+    }
+
+    // Getter for toppingPrice
+    public double getToppingPrice() {
+        return toppingPrice;
+    }
+
+    // Setter for toppingPrice (not typical for enums)
+    public void setToppingPrice(double toppingPrice) {
+        this.toppingPrice = toppingPrice;
+    }
 }
+		

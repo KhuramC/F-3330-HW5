0965b3e77c6ce9e875ba0d66abd304d016090fb1
KhuramC <khuramchoudhry123@gmail.com>
2024-03-07 23:06:52 -0600

    Implemented CookingStyleType enum (with double values and getter).

--- Homework_5/src/pizzaMakingStrategies/CookingStyleType.java
+++ Homework_5/src/pizzaMakingStrategies/CookingStyleType.java
@@ -1,5 +1,20 @@
 package pizzaMakingStrategies;
 
 public enum CookingStyleType {
+	
+	MICROWAVE(1.0),
+	CONVENTIONAL_OVEN(8.0),
+	BRICK_OVEN(10.0);
 
+	private double cookingPrice;
+	
+	//Constructor//
+	CookingStyleType(double cookingPrice) {
+		this.cookingPrice = cookingPrice;
+	}
+	
+	//Getter//
+	public double getCookingPrice() {
+		return cookingPrice;
+	}
 }

30cef61e1a251adb020b346eef692b7d01c26459
Alon-Barzilay <alonbarzilay0@gmail.com>
2024-03-09 16:26:12 -0600

    Added createPizza method.

--- Homework_5/src/pizzaManagement/PizzaCookingFactory.java
+++ Homework_5/src/pizzaManagement/PizzaCookingFactory.java
@@ -2,6 +2,7 @@ package pizzaManagement;
 import pizzas.*;
 
 public class PizzaCookingFactory {
+	static int orderIDCounter = 1;
 
 	public PizzaCookingFactory() {
 		// TODO Auto-generated constructor stub
@@ -13,6 +14,28 @@ public class PizzaCookingFactory {
 	using the current orderIDCounter of the pizza. The orderIDCounter will be incremented and
 	assigned automatically on every creation of pizza, to assign unique pizza order IDs. */
 	public AbstractPizza createPizza(PizzaType pizzaType) {
-		return null;
+		AbstractPizza pizza = null;
+		
+		switch(pizzaType) {
+		case MARGHERITA:
+			pizza = new MargheritaPizza(orderIDCounter);
+			break;
+		case HAWAIIAN:
+			pizza = new HawaiianPizza(orderIDCounter);
+			break;
+		case SUPREME:
+			pizza = new SupremePizza(orderIDCounter);
+			break;
+		case VEGETARIAN:
+			pizza = new VegetarianPizza(orderIDCounter);
+			break;
+		}
+		
+		if (pizza != null) {
+            pizza.setPizzaOrderID(orderIDCounter);
+            orderIDCounter++; // Increment for the next order
+        }
+		
+		return pizza;
 	}
 }

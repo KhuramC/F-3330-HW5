ecf7d04c2c2471f7f6bccf257a84ce9f42d11613
RoneyTrenton <tjr_1st@hotmail.com>
2024-03-12 20:04:39 -0500

    Added code to Main.java for testing purposes
    Added two more checkouts to help show the increase/decrease in price
    when a topping is added/removed.

--- Homework_5/src/testing/Main.java
+++ Homework_5/src/testing/Main.java
@@ -36,7 +36,7 @@ public class Main {
         }
 
         // Commented out until needed ----- Test for adding new toppings
-//        order.addNewToppingToPizza(1, Toppings.BLACK_OLIVE);
+        order.addNewToppingToPizza(1, Toppings.BLACK_OLIVE);
 //        order.addNewToppingToPizza(1, Toppings.MUSHROOM);
 //        order.addNewToppingToPizza(2, Toppings.CANADIAN_BACON);
 //
@@ -44,13 +44,33 @@ public class Main {
         System.out.println("\nPizza Order Cart after the added toppings:");
         order.printPizzaOrderCart(1);
 //
+     // A second checkout to show that when BlackOlives are added, the price is increased.
+        try {
+            // Attempt to checkout (calculate the total price)
+            double totalPrice = order.checkout();
+            System.out.println("\nCheckout Success. ----- Total Price: $" + totalPrice);
+        } catch (Exception e) {
+            // Exception (uncooked pizzas)
+            System.out.println("\nError upon checkout " + e.getMessage());
+        }
+        
         // Commented out until needed ----- Test for removing new toppings
 //        order.removeToppingFromPizza(1, Toppings.BLACK_OLIVE);
-//        order.removeToppingFromPizza(2, Toppings.MUSHROOM);
+        order.removeToppingFromPizza(2, Toppings.MUSHROOM);
 
         System.out.println("\nPizza Order Cart after the removed toppings:");
         order.printPizzaOrderCart(1);
 
+        // A third checkout to show that when Mushrooms are removed, price is decreased.
+        try {
+            // Attempt to checkout (calculate the total price)
+            double totalPrice = order.checkout();
+            System.out.println("\nCheckout Success. ----- Total Price: $" + totalPrice);
+        } catch (Exception e) {
+            // Exception (uncooked pizzas)
+            System.out.println("\nError upon checkout " + e.getMessage());
+        }
+        
         // Printing toppings by OrderID. As well as an order ID that should not be found (3).
         System.out.println("\n");
         order.printListOfToppingsByPizzaOrderID(1);
